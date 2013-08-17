ce35022ff4db8627f422d98158f9ff07a255c1fc
yuanchi <eeit54@gmail.com>
2013-08-16 20:36:30 -0700

    build testing show page

--- common/myGlobal.js
+++ common/myGlobal.js
@@ -2,4 +2,29 @@
 	var MyGlobal = function(){};
 	//...other added functionality
 	host.myGlobal = host.myGlobal || new MyGlobal();
-})(this)
+	alert('success');
+})(this);
+//nonblocking JavaScript download
+(function(host){
+	host.myGlobal.loadScript = function(url, callback){
+		var script = document.createElement('script');
+		script.type = 'text/javascript',
+		readyState = script.readyState;
+		
+		if(readyState){// for IE
+			script.onreadystatechange = function(){
+				if(readyState == 'load' || readyState == 'complete'){
+					script.onreadystatechange = null;
+					callback();
+				}
+			}
+		}else{
+			script.onload = function(){
+				callback();
+			};
+		}
+		
+		script.src = url;
+		document.getElementsByTagName('head')[0].appendChild(script);
+	};
+})(this);
